c96fc94f3af1cf94bab017d5c36e7718e3017eb8
andle7 <hb1809@naver.com>
2024-06-14 21:50:25 +0900

    [Add]p10039 -java

---
+++ java/solved.ac/src/R1_bronze/p10039.java
@@ -0,0 +1,24 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p10039 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int arr[] = new int[5];
+		int sum = 0;
+		int avg = 0;
+		
+		for(int i=0; i<5; i++) {
+			arr[i] = in.nextInt();
+			if(arr[i] < 40) {
+				arr[i] = 40;
+			}
+			sum +=arr[i];
+		}
+		
+		avg = (int)(sum/5.0);
+		
+		System.out.println(avg);
+	}
+}
