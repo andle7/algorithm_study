cfe9547cf95f3630203793208ec48be89d4a0cdb
andle7 <hb1809@naver.com>
2024-06-15 10:16:34 +0900

    [Add]2024/06/15 p2711 -java

---
+++ java/solved.ac/src/R1_bronze/p2711.java
@@ -0,0 +1,24 @@
+package R1_bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p2711 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine().trim());
+		
+		String[][] arr = new String[N][2];
+		
+		for(int i = 0; i<N; i++) {
+			String[] input = br.readLine().split(" ");
+			arr[i][0] = input[0];
+			arr[i][1] = input[1];
+			
+			int indexToRemove = Integer.parseInt(arr[i][0]) - 1;
+		    arr[i][1] = arr[i][1].substring(0, indexToRemove) + arr[i][1].substring(indexToRemove + 1);             
+			System.out.println(arr[i][1]);
+		}	
+	}
+}
