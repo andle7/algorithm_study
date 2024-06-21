e0ce9b87e84daac64e51ee094b9e9ef87fc3f0df
andle7 <hb1809@naver.com>
2024-06-21 11:17:14 +0900

    2024/06/21 p15000 -java

---
+++ java/solved.ac/src/R1_bronze/p15000.java
@@ -0,0 +1,12 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p15000 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String st = in.nextLine();
+		
+		System.out.println(st.toUpperCase());
+	}
+}
