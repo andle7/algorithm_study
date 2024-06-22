005a33bbd81c0c4e0814863fc145d1deac71629c
andle7 <hb1809@naver.com>
2024-06-23 06:22:24 +0900

    [add]2024/06/23 p26082 -java

---
+++ java/solved.ac/src/R1_bronze/p26082.java
@@ -0,0 +1,17 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p26082 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		int C = in.nextInt();
+		
+		int result = B/A*3*C;
+		
+		System.out.println(result);
+		
+	}
+}
