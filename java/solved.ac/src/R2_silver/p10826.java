e50c8ed1dd8935c9233faa34ac702e88d08462bc
andle7 <hb1809@naver.com>
2024-06-24 06:37:30 +0900

    [add]2024/06/24 p10826 -java

---
+++ java/solved.ac/src/R2_silver/p10826.java
@@ -0,0 +1,37 @@
+package R2_silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class p10826 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        BigInteger bt = new BigInteger(br.readLine());
+        
+        int N = bt.intValue();
+        
+        System.out.println(fibonacci(N));
+    }
+    
+    static BigInteger fibonacci(int N) {
+        if (N == 0) return 
+        		BigInteger.ZERO;
+        if (N == 1) return 
+        		BigInteger.ONE;
+        
+        BigInteger a = BigInteger.ZERO;
+        BigInteger b = BigInteger.ONE;
+        BigInteger c = BigInteger.ZERO;
+        
+        for (int i = 2; i <= N; i++) {
+            c = a.add(b);
+            a = b;
+            b = c;
+        }
+        
+        return c;
+    }
+}
