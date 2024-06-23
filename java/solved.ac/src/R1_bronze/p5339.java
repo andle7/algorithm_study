e1ca37372e2bec7467c31f06cecb376d83199360
andle7 <hb1809@naver.com>
2024-06-24 01:07:23 +0900

    [add]2024/06/24 p5339 -java

---
+++ java/solved.ac/src/R1_bronze/p5339.java
@@ -0,0 +1,16 @@
+package R1_bronze;
+
+public class p5339 {
+    public static void main(String[] args) {
+        System.out.printf("     /~\\\n");
+        System.out.printf("    ( oo|\n");
+        System.out.printf("    _\\=/_\n");
+        System.out.printf("   /  _  \\\n");
+        System.out.printf("  //|/.\\|\\\\\n");
+        System.out.printf(" ||  \\ /  ||\n");
+        System.out.printf("============\n");
+        System.out.printf("|          |\n");
+        System.out.printf("|          |\n");
+        System.out.printf("|          |\n");
+    }
+}
