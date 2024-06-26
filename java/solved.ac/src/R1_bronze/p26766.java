420084c9dbdbf25d56569e6d514c825445515cbb
andle7 <hb1809@naver.com>
2024-06-26 23:02:48 +0900

    [add]2024/06/26 p26766 -java

---
+++ java/solved.ac/src/R1_bronze/p26766.java
@@ -0,0 +1,27 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p26766 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		
+		for(int i = 0; i < N; i++ ) {
+				hart();
+		}
+
+	}
+
+	public static void hart() {
+	    System.out.println(" @@@   @@@ ");
+        System.out.println("@   @ @   @");
+        System.out.println("@    @    @");
+        System.out.println("@         @");
+        System.out.println(" @       @ ");
+        System.out.println("  @     @  ");
+        System.out.println("   @   @   ");
+        System.out.println("    @ @    ");
+        System.out.println("     @     ");
+	}
+}
