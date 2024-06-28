88aca7b4fd7e8e2016e92cc014527f1b7f374961
andle7 <hb1809@naver.com>
2024-06-28 10:02:39 +0900

    [add]2024/06/28 p동물의 아이디와 이름 -mysql

---
+++ programmers/MySQL/p동물의 아이디와 이름.sql
@@ -0,0 +1,16 @@
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS;
+
+
+
+
+
+/*
+1단계
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS;
+
+
+*/
+
+/* https://school.programmers.co.kr/learn/courses/30/lessons/59403 */
