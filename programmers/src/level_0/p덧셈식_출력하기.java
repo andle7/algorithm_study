7efdeedb7f9424ab8c32f4209df19c590cb185a6
andle7 <hb1809@naver.com>
2024-06-29 00:08:40 +0900

    [add]2024/06/29 p덧셈식 출력하기 -java

---
+++ programmers/src/level_0/p덧셈식_출력하기.java
@@ -0,0 +1,13 @@
+package level_0;
+
+import java.util.Scanner;
+
+public class p덧셈식_출력하기 {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
