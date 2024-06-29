4a47d991ab99af82097c64cee3b9780d3ad92a1f
andle7 <hb1809@naver.com>
2024-06-30 08:07:59 +0900

    [add]2024/06/30 p문자 리스트를 문자열로 변환하기 -java

---
+++ programmers/src/level_0/p문자_리스트를_문자열로_변환하기.java
@@ -0,0 +1,13 @@
+package level_0;
+
+public class p문자_리스트를_문자열로_변환하기 {
+	 public String solution(String[] arr) {
+	        String answer = "";
+	        
+	        for(int i = 0; i<arr.length; i++){
+	            answer += arr[i];
+	        }
+	        
+	        return answer;
+	    }
+}
