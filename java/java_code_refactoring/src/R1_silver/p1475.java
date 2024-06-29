82299654e7f0ba2e5f4ca5b2866df3f65db14b88
andle7 <hb1809@naver.com>
2024-06-29 10:56:01 +0900

    [add]2024/06/29 p1475 -java

---
+++ java/java_code_refactoring/src/R1_silver/p1475.java
@@ -0,0 +1,33 @@
+package R1_silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class p1475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        int[] num = new int[9];
+        
+        for (char ch : N.toCharArray()) {
+            int i = ch - '0';
+            if (i == 9 || i == 6) {
+                num[6]++;
+            } else {
+                num[i]++;
+            }
+        }
+        
+        num[6] = (num[6] + 1) / 2;
+        
+        int max = 0;
+        for (int count : num) {
+            if (count > max) {
+                max = count;
+            }
+        }
+        
+        System.out.println(max);
+    }
+}
---
+++ java/solved.ac/src/R2_silver/p1475.java
@@ -0,0 +1,29 @@
+package R2_silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.io.IOException;
+
+public class p1475 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String N = br.readLine();
+		int[] num = new int[10];
+		
+		for(char ch : N.toCharArray()) {
+			int i = ch - '0';
+			if(i==9) { 
+				i = 6;
+			}
+			num[i]++;
+		}
+		
+		num[6] = num[6]/2 + num[6]%2;
+		
+		Arrays.sort(num);
+		
+		System.out.println(num[9]);
+
+	}
+}
