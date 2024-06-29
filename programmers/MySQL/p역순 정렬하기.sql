f786af847f844e90c1977cd3b7e98b6ed163f28c
andle7 <hb1809@naver.com>
2024-06-29 10:12:07 +0900

    [add]2024/06/29 p역순 정렬하기 -mysql

---
+++ programmers/MySQL/p역순 정렬하기.sql
@@ -0,0 +1,13 @@
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC;
+
+/*
+1단계
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC;
+
+ */
+
+/* https://school.programmers.co.kr/learn/courses/30/lessons/59035 */
