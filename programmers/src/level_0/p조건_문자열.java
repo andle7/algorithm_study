c5a4b9f8593d4dfd2e8f039c75099af77b3278ab
andle7 <hb1809@naver.com>
2024-07-02 01:06:48 +0900

    [add]2024/07/02 p조건 문자열 -java

---
+++ programmers/src/level_0/p조건_문자열.java
@@ -0,0 +1,35 @@
+package level_0;
+
+public class p조건_문자열 {
+	public int solution(String ineq, String eq, int n, int m) {
+		int answer = 0;
+
+		if (ineq.equals(">") && eq.equals("=")) {
+			if (n >= m) {
+				answer = 1;
+			} else {
+				answer = 0;
+			}
+		} else if (ineq.equals("<") && eq.equals("=")) {
+			if (n <= m) {
+				answer = 1;
+			} else {
+				answer = 0;
+			}
+		} else if (ineq.equals(">") && eq.equals("!")) {
+			if (n > m) {
+				answer = 1;
+			} else {
+				answer = 0;
+			}
+		} else if (ineq.equals("<") && eq.equals("!")) {
+			if (n < m) {
+				answer = 1;
+			} else {
+				answer = 0;
+			}
+		}
+
+		return answer;
+	}
+}
