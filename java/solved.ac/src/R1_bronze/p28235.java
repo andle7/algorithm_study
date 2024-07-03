e1e3a067b3f2915e748febf4c8b3d1028299594d
andle7 <hb1809@naver.com>
2024-07-04 07:25:29 +0900

    [add]2024/07/04 p28235 -java

---
+++ java/solved.ac/src/R1_bronze/p28235.java
@@ -0,0 +1,29 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p28235 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String st = in.nextLine().toUpperCase();
+		
+		switch(st) {
+		case "SONGDO" :
+			System.out.println("HIGHSCHOOL");
+			break;
+			
+		case "CODE" :
+			System.out.println("MASTER");
+			break;
+			
+		case "2023" :
+			System.out.println("0611");
+			break;
+		case "ALGORITHM" :
+			System.out.println("CONTEST");
+			break;
+			
+		}
+		
+	}
+}
