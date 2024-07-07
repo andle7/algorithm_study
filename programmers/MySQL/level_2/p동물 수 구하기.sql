0d050eae8f032dd2e70535658959ae11b03d8ead
andle7 <hb1809@naver.com>
2024-07-07 21:26:18 +0900

    [add]2024/07/07 p동물 수 구하기 -mysql

---
+++ programmers/MySQL/level_2/p동물 수 구하기.sql
@@ -0,0 +1,15 @@
+SELECT COUNT(*) AS count
+FROM ANIMAL_INS;
+
+/*
+1단계
+SELECT ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS;
+
+2단계
+SELECT COUNT(*) AS count
+FROM ANIMAL_INS;
+
+*/
+
+/* https://school.programmers.co.kr/learn/courses/30/lessons/59406 */
