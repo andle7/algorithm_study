37c056641fc8a9da6de20ff78417bc9c640f8827
andle7 <hb1809@naver.com>
2024-07-09 06:18:23 +0900

    [re]2024/07/09 p8370 -java

---
+++ java/java_code_refactoring/src/R1_bronze/p8370_1.java
@@ -0,0 +1,16 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p8370_1 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n1 = in.nextInt();
+		int k1 = in.nextInt();
+		int n2 = in.nextInt();
+		int k2 = in.nextInt();
+		
+		System.out.println(n1*k1 + n2*k2);
+
+	}
+}
