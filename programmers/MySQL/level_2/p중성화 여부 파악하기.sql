cd587f12f306f290bc6aaacc00fbda52ecdd5d11
andle7 <hb1809@naver.com>
2024-07-08 09:33:29 +0900

    [add]2024/07/08 p중성화 여부 파악하기 -mysql

---
+++ programmers/MySQL/level_2/p중성화 여부 파악하기.sql
@@ -0,0 +1,26 @@
+SELECT ANIMAL_ID, NAME, IF(SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%', 'O', 'X') AS 중성화
+FROM ANIMAL_INS;
+
+/*
+1단계
+SELECT ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS;
+
+2단계
+//1. IF 사용
+SELECT ANIMAL_ID, NAME, IF(SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%', 'O', 'X') AS 중성화
+FROM ANIMAL_INS;
+
+
+//2. CASE 사용
+SELECT ANIMAL_ID, NAME,
+    CASE 
+        WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+        ELSE 'X'
+    END AS 중성화
+FROM ANIMAL_INS;
+
+
+*/
+
+/* https://school.programmers.co.kr/learn/courses/30/lessons/59409 */
