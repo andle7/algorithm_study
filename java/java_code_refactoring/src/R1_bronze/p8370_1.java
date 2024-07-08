3c33f6d1ec4986eb49136c69c84e22e4eef794e3
andle7 <hb1809@naver.com>
2024-07-09 06:36:25 +0900

    [add]2024/07/09 p조건에 맞는 도서와 저자 리스트 출력하기 -mysql

--- java/java_code_refactoring/src/R1_bronze/p8370_1.java
+++ java/java_code_refactoring/src/R1_bronze/p8370_1.java
@@ -1,14 +1,17 @@
 package R1_bronze;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class p8370_1 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n1 = in.nextInt();
-		int k1 = in.nextInt();
-		int n2 = in.nextInt();
-		int k2 = in.nextInt();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int n1 = Integer.parseInt(input[0]);
+		int k1 = Integer.parseInt(input[1]);
+		int n2 = Integer.parseInt(input[2]);
+		int k2 = Integer.parseInt(input[3]);
 		
 		System.out.println(n1*k1 + n2*k2);
 
--- java/solved.ac/src/R1_bronze/p8370.java
+++ java/solved.ac/src/R1_bronze/p8370.java
@@ -1,17 +1,14 @@
 package R1_bronze;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class p8370 {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
-		int n1 = Integer.parseInt(input[0]);
-		int k1 = Integer.parseInt(input[1]);
-		int n2 = Integer.parseInt(input[2]);
-		int k2 = Integer.parseInt(input[3]);
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n1 = in.nextInt();
+		int k1 = in.nextInt();
+		int n2 = in.nextInt();
+		int k2 = in.nextInt();
 		
 		System.out.println(n1*k1 + n2*k2);
 
