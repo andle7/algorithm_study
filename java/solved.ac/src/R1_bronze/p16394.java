5e4d5a99cb98fa9eb9a27792c6622dc6396561b7
andle7 <hb1809@naver.com>
2024-07-10 07:21:21 +0900

    [add]2024/07/10 p16394 -java

---
+++ java/solved.ac/src/R1_bronze/p16394.java
@@ -0,0 +1,13 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p16394 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		
+		System.out.println(n -1946);
+		
+	}
+}
