b1278448bd0446479cc3ead61b06e08e94bc1e2d
andle7 <hb1809@naver.com>
2024-07-14 16:57:47 +0900

    [add]2024/07/14 p1940 -java

---
+++ java/solved.ac/src/R2_silver/p1940.java
@@ -0,0 +1,44 @@
+package R2_silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class p1940 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		int count = 0;
+		int i = 0;
+		int j = N - 1;
+		
+		while(i < j) {
+			if(arr[i] + arr[j] < M) {
+				i++;
+			} else if(arr[i] + arr[j] > M) {
+				j--;
+			} else {
+				count++;
+				i++;
+				j--;
+			}
+		}
+		
+		System.out.println(count);
+		br.close();
+		
+	}
+}
