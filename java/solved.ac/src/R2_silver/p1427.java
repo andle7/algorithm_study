200016fc1ace777b172cce8bfca358467e84b8ef
andle7 <hb1809@naver.com>
2024-07-27 23:05:38 +0900

    [add]2024/07/27 p1427 -java

--- java/solved.ac/src/R2_silver/p1427.java
+++ java/solved.ac/src/R2_silver/p1427.java
@@ -1,19 +1,20 @@
 package R2_silver;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class p1427 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String st = in.next();
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String st = br.readLine();
 		int[] arr = new int[st.length()];
 		
 		for(int i=0; i<st.length(); i++) {
 			arr[i] = Integer.parseInt(st.substring(i, i + 1));
 		}
 		
-		for(int i = 0; i< st.length(); i++) {
+		for(int i = 0; i < st.length(); i++) {
 			int Max = i;
 			for(int j = i + 1; j < st.length(); j++) {
 				if(arr[j] > arr[Max])
@@ -24,11 +25,10 @@ public class p1427 {
 				arr[i] = arr[Max];
 				arr[Max] = tmp;
 			}
-		}
+		}	
 		
 		for(int i = 0; i < st.length(); i++) {
 			System.out.print(arr[i]);
 		}
-		
 	}
 }
