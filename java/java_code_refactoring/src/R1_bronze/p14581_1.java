959121f847c71314619a8585ae265cf204ee0f3c
andle7 <hb1809@naver.com>
2024-08-05 11:06:23 +0900

    [add]2024/08/05 p14581 -java

---
+++ java/java_code_refactoring/src/R1_bronze/p14581_1.java
@@ -0,0 +1,20 @@
+package R1_bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p14581_1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String hong = br.readLine();
+		br.close();
+
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + hong + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+
+}
---
+++ java/solved.ac/src/R1_bronze/p14581.java
@@ -0,0 +1,17 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p14581 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String hong = in.nextLine();
+		in.close();
+
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + hong + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+
+}
