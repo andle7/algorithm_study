777af216e3e7e5a241a0134e8a3b899e82df5557
andle7 <hb1809@naver.com>
2024-08-06 09:25:16 +0900

    [add]2024/08/06 p15963 -java

---
+++ java/solved.ac/src/R1_bronze/p15963.java
@@ -0,0 +1,18 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p15963 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		long N = in.nextLong();
+		long M = in.nextLong();
+		
+		if(N == M)
+			System.out.println("1");
+		else
+			System.out.println("0");
+		
+	}
+}
