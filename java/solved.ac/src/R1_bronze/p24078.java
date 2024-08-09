3bc643bd72174a39a2ad6fd63f5fbfd127df6e9d
andle7 <hb1809@naver.com>
2024-08-09 11:13:27 +0900

    [add]2024/08/09 p24078 -java

---
+++ java/solved.ac/src/R1_bronze/p24078.java
@@ -0,0 +1,14 @@
+package R1_bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class p24078 {
+	 public static void main(String[] args) throws Exception {
+		 int MOD = 21;
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        System.out.println(Integer.parseInt(br.readLine()) % MOD);
+	 }
+}
+
+
+
