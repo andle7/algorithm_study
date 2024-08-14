632f9af5df31d033bd2c8dffaa7bb7c3c512f2b6
andle7 <hb1809@naver.com>
2024-08-15 00:12:20 +0900

    [add]2024/08/15 p8871 -java

---
+++ java/solved.ac/src/R1_bronze/p8871.java
@@ -0,0 +1,11 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p8871 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		System.out.printf("%d %d", (n + 1) * 2, (n + 1) * 3);
+	}
+}
