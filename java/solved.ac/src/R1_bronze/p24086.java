acf29ef6429706fb8cf3d1048371a09fb51cc12c
andle7 <hb1809@naver.com>
2024-09-09 09:28:13 +0900

    [add]2024/09/09 p24086 -java

---
+++ java/solved.ac/src/R1_bronze/p24086.java
@@ -0,0 +1,12 @@
+package R1_bronze;
+
+import java.util.Scanner;
+public class p24086 {
+    public static void main(String[] args){
+    	
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(b-a);
+    }
+}
