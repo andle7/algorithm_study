2ad9307b520a31f277bc1b7cc6a03c3069497da3
andle7 <hb1809@naver.com>
2024-11-14 21:10:45 +0900

    [add]2024/11/14 p6810 -java

---
+++ java/solved.ac/src/R1_bronze/p6810.java
@@ -0,0 +1,18 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p6810 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        System.out.print("The 1-3-sum is ");
+        System.out.print(91 +a*1 + b*3+c*1);
+
+    }
+}
