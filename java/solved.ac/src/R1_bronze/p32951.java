e3e609ee817c7500ba441e71d429390e611df451
andle7 <hb1809@naver.com>
2025-01-10 14:48:42 +0900

    [add]2025/01/10 p32951 -java

---
+++ java/solved.ac/src/R1_bronze/p32951.java
@@ -0,0 +1,13 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p32951 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int year = in.nextInt();
+		
+		System.out.println(year - 2024);
+	}
+}
