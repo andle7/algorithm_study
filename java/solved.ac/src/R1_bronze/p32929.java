f40b2b19c75ebaf0f7dca22a3740b95b16f29f0b
andle7 <hb1809@naver.com>
2025-01-12 11:33:46 +0900

    [add]2025/01/12 p32929 -java

---
+++ java/solved.ac/src/R1_bronze/p32929.java
@@ -0,0 +1,9 @@
+package R1_bronze;
+
+import java.util.Scanner;
+
+public class p32929 {
+    public static void main(String[] args) {
+        System.out.println("SUO".charAt(new Scanner(System.in).nextInt() % 3));
+    }
+}
